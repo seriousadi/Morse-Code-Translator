c0cac8cb8c61182e85a3d945dca480fd5023b580
Aditya Kumar <aaz22hsns@gmail.com>
2023-07-14 11:09:33 +0530

    Improve functionality

---
+++ morse code/get_morse_code.py
@@ -0,0 +1,24 @@
+from morsecode import MorseCode
+
+morse_codes = MorseCode().morse_code
+
+
+class GetMorseCode():
+    def __init__(self):
+        pass
+
+    def convert_morse_code(self, string: str):
+        string = string.upper()
+        for n in string:
+            try:
+                if n != " ":
+                    string = string.replace(n, morse_codes[n])
+
+                elif n == " ":
+                    string = string.replace(n, "space")
+
+            except KeyError:
+                print(f"this({n}) isn't in morse code, \n"
+                      f"Morse code have limited amount of symbols, cause proper grammar isn't first priority in war")
+
+        return string
--- morse code/main.py
+++ morse code/main.py
@@ -1,24 +1,7 @@
-from MORSECODE import MorseCode
+from get_morse_code import GetMorseCode
 
-morse_codes = MorseCode().morse_code
-
-
-class GetMorseCode():
-    def __init__(self):
-        pass
-
-    def convert_morse_code(self, string: str):
-        string = string.upper()
-        for n in string:
-            try:
-                if n != " ":
-                    string = string.replace(n, morse_codes[n])
-
-                elif n == " ":
-                    string = string.replace(n, "space")
-
-            except KeyError:
-                print(f"this({n}) isn't in morse code, \n"
-                      f"Morse code have limited amount of symbols, cause proper grammar isn't first priority in war")
-
-        return string
+morse_code_generator = GetMorseCode()
+print("The symbol/letter which can't be converted will be returned as it is")
+to_translate = input("What do you want to translate:  ")
+translated_code = morse_code_generator.convert_morse_code(to_translate)
+print(f"Your translated code:  {translated_code}")
